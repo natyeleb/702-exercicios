38cd7d37cf69b8a41f973f3f8e4feb802f5b6549
natyele <natyelebertolanii@gmail.com>
2019-08-13 17:45:53 -0300

    adicionar exercicios

---
+++ calendario.py
@@ -0,0 +1,19 @@
+numero = int(input("digite um numero de 1 a 7: "))
+
+if numero == 1:
+    print('é domingo!')
+elif numero == 2:
+    print('é segunda-feira!')
+elif numero == 3:
+    print('é terça-feira!')
+elif numero == 4:
+    print('é quarta-feira!')
+elif numero == 5:
+    print('é quinta-feira!')
+elif numero == 6:
+    print('é sexta!')
+elif numero == 7:
+    print('é sabado!')
+else:
+    print('Digitou um numero invalido')
+    
---
+++ funcionario1.py
@@ -0,0 +1,8 @@
+nome = input("digite seu nome: ")
+salario = float(input("digite quantas horas voce trabalhou: "))
+hora = float(input("digite quanto voce recebe por hora: ")) 
+
+salario =(hora * salario)     
+
+print("Seu salario é:" +str(salario))
+
